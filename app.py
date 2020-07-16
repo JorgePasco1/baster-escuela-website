b3ff160a1a56c82bbe9adef1431aa0b23a12dcd1
jorgepasco1 <jorgepascosoto@gmail.com>
2020-07-16 00:12:31 -0500

    add common layout and header
    also fix typos

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
+
+
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -11,6 +13,12 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html", active="about")
